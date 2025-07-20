ed1d132321e457563e4ec312f178af775264c47a
Roman <googlik999@yandex.ru>
2025-07-21 01:33:08 +0300

    ParticipationRequestServiceImpl

--- main/src/main/java/ru/practicum/service/impl/ParticipationRequestServiceImpl.java
+++ main/src/main/java/ru/practicum/service/impl/ParticipationRequestServiceImpl.java
@@ -3,24 +3,43 @@ package ru.practicum.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 import ru.practicum.dto.request.EventRequestStatusUpdateRequest;
 import ru.practicum.dto.request.EventRequestStatusUpdateResult;
 import ru.practicum.dto.request.ParticipationRequestDto;
+import ru.practicum.entity.Event;
+import ru.practicum.entity.ParticipationRequest;
+import ru.practicum.entity.RequestStatus;
+import ru.practicum.exception.ConflictException;
+import ru.practicum.exception.NotFoundException;
+import ru.practicum.mapper.ParticipationRequestMapper;
+import ru.practicum.repository.EventRepository;
+import ru.practicum.repository.ParticipationRequestRepository;
 import ru.practicum.service.ParticipationRequestService;
 
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ParticipationRequestServiceImpl implements ParticipationRequestService {
 
+    private final ParticipationRequestRepository requestRepository;
+    private final EventRepository eventRepository;
+    private final ParticipationRequestMapper participationRequestMapper;
+
     @Override
     public List<ParticipationRequestDto> getRequestForEventByUserId(Long eventId, Long userId) {
         log.info("Get request for event by id: {}", eventId);
-        // todo
-        return List.of();
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new NotFoundException("Event id" + eventId + "not found"));
+        if (!Objects.equals(event.getInitiator().getId(), userId)) {
+            throw new ConflictException("Can't get request for event id=" + eventId + "by user id=" + userId);
+        }
+        List<ParticipationRequest> requests = requestRepository.findAllByEventId(eventId);
+        return requests.stream()
+                .map(participationRequestMapper::toDto)
+                .toList();
     }
 
     @Override
@@ -28,8 +47,55 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
                                                          Long userId,
                                                          EventRequestStatusUpdateRequest updateRequest) {
         log.info("Update request: {}", updateRequest);
-        // todo
-        return null;
+        List<ParticipationRequest> requestList = requestRepository.findAllById(updateRequest.getRequestIds());
+        Event event = eventRepository
+                .findById(eventId).orElseThrow(() -> new NotFoundException("There is no event id=" + eventId));
+        if (!Objects.equals(event.getInitiator().getId(), userId)) {
+            throw new ConflictException("Can't update event id=" + eventId + " requests by user id=" + userId);
+        }
+        updateRequests(requestList, updateRequest.getStatus(), event);
+
+        EventRequestStatusUpdateResult result = new EventRequestStatusUpdateResult();
+        requestList.forEach(request -> {
+            switch (request.getStatus()) {
+                case RequestStatus.REJECTED ->
+                        result.getRejectedRequests().add(participationRequestMapper.toDto(request));
+                case RequestStatus.CONFIRMED ->
+                        result.getConfirmedRequests().add(participationRequestMapper.toDto(request));
+            }
+        });
+        return result;
+    }
+
+    private void updateRequests(List<ParticipationRequest> requests, RequestStatus status, Event event) {
+        boolean hasNotPendingRequests = requests.stream().map(ParticipationRequest::getStatus).anyMatch(el -> el != RequestStatus.PENDING);
+        if (hasNotPendingRequests)
+            throw new ConflictException("Can't change status when request status is not PENDING");
+
+        if (status == RequestStatus.REJECTED) {
+            for (ParticipationRequest request : requests) {
+                request.setStatus(RequestStatus.REJECTED);
+            }
+            return;
+        }
+        Boolean requestModeration = event.getRequestModeration();
+        Integer participantLimit = event.getParticipantLimit();
+
+        if (!requestModeration && participantLimit == null) {
+            requests.forEach(request -> request.setStatus(status));
+            return;
+        }
+
+        long confirmed = requestRepository
+                .countByEventIdAndStatus(event.getId(), RequestStatus.CONFIRMED);
+        for (ParticipationRequest request : requests) {
+            if (confirmed >= participantLimit) {
+                throw new ConflictException("Requests out of limit");
+            } else {
+                request.setStatus(status);
+                confirmed++;
+            }
+        }
     }
 
 }
