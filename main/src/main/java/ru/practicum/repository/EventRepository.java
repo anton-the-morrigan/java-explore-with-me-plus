9091feba4e98598131353a6af7455339bf482dae
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-07-23 04:01:25 +0300

    Revert "Main svc compilations (#8)" (#10)
    This reverts commit c8f5f9264b86f559cf2ac83ede5f3f1307af8e77.

--- main/src/main/java/ru/practicum/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.entity.Event;
-
-@Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
