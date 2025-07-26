cd8674576192ced2c412672a5119b5600c027a55
VurtsGD <vgd2001@yandex.ru>
2025-07-26 21:58:45 +0600

    stats service возвращает код 400 на запрос GET /stats когда время старта > время окончания

--- stats/stats-server/src/main/java/ru/practicum/stats/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/StatsController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 
@@ -26,6 +27,13 @@ public class StatsController {
                                        @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                        @RequestParam(required = false) List<String> uris,
                                        @RequestParam(defaultValue = "false") boolean unique) {
+        if (end.isBefore(start)) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "startTime must be before endTime"
+            );
+        }
+
         return statsService.getStats(start, end, uris, unique);
     }
 }
